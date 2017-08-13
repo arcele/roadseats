aef37d4c82e0223762a6362e2577b96ebade1561
Ryan Kelley <ryan.kelley@espn.com>
2017-08-13 19:01:29 -0400

    save coordinates as objects in state

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,28 @@
-export const chooseTeam = (teamId) => ({
+function parseCoordinates(xyString) {
+	// turn coordinates in string format '47.61W,122.33N' into coordinate object
+	let xyObject = {}
+	const splitString = xyString.split(',')
+	if(splitString.length == 2) {
+		xyObject = {
+			latitude : {
+				radians: splitString[0].slice(0,splitString[0].length-1),
+				direction: splitString[0].slice(-1)
+			},
+			longitude: {
+				radians: splitString[1].slice(0,splitString[1].length-1),
+				direction: splitString[1].slice(-1)
+			}
+		}
+	}
+	return xyObject;
+}
+
+export const chooseTeam = (teamLocation) => ({
 	type: 'TEAM_CHOSEN',
-	teamId
+	teamLocation: parseCoordinates(teamLocation)
 })
 
-export const chooseLocation = (coordinates) => ({
+export const chooseLocation = (searchCoordinates) => ({
 	type: 'LOCATION_CHOSEN',
-	location: coordinates
+	searchLocation: parseCoordinates(searchCoordinates)
 })
