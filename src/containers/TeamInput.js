4051d70e822e45ff360dd72fb5c850663d6fb403
Ryan Kelley <ryan.kelley@espn.com>
2017-08-13 19:19:55 -0400

    fix event indentation

--- src/containers/TeamInput.js
+++ src/containers/TeamInput.js
@@ -9,13 +9,13 @@ let TeamInput = ({ dispatch }) => {
 			<form>
 				<select ref={node => { input = node }}
 					onChange={e => {
-					e.preventDefault()
-					if(input.value === -1) {
-						return
-					}
-					console.log('chooseTeam ->', input.value)
-					dispatch(chooseTeam(input.value))
-				}}>
+						e.preventDefault()
+						if(input.value === -1) {
+							return
+						}
+						console.log('chooseTeam ->', input.value)
+						dispatch(chooseTeam(input.value))
+					}}>
 					<option value="-1"> -- Your Team --</option>
 					<option value="33.75528N,-84.40099W">Atlanta Falcons</option>
 					<option value="42.77378N,-78.78695W">Buffalo Bills</option>
