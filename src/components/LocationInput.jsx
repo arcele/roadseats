9995046d0b2ea6ae8003e4cf5a5b16a26e7b81d4
Ryan Kelley <ryan.kelley@espn.com>
2017-08-15 18:42:27 -0400

    jsx all the things

---
+++ src/components/LocationInput.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { chooseLocation } from '../actions'
+
+class LocationInput extends Component {
+	render () {
+		let input
+		return (
+			<form>
+				<select ref={node => { input = node }}
+					onChange={ e => {
+						e.preventDefault()
+						if(input.value === -1) {
+							return false
+						}
+						console.log('chooseLocation >', input.value)
+						this.props.dispatch(chooseLocation(input.value))
+					}}>
+					<option value="-1"> -- Your Location --</option>
+					<option value="30.2672N,97.7431W">Austin, TX</option>
+					<option value="47.6062N,122.3321W">Seattle, WA</option>
+					<option value="41.7637N,72.6851W">Hartford, CT</option>
+				</select>
+
+
+			</form>
+		)
+	}
+}
+LocationInput = connect()(LocationInput)
+
+export default LocationInput
---
+++ src/components/TeamInput.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { chooseTeam } from '../actions'
+
+class TeamInput extends Component {
+	render() {
+		let input
+		return (
+			<div>
+				<form>
+					<select ref={node => { input = node }}
+						onChange={e => {
+							e.preventDefault()
+							if(input.value === -1) {
+								return
+							}
+							console.log('chooseTeam ->', input.value)
+							this.props.dispatch(chooseTeam(input.value))
+						}}>
+						<option value="-1"> -- Your Team --</option>
+						<option value="33.75528N,84.40099W">Atlanta Falcons</option>
+						<option value="42.77378N,78.78695W">Buffalo Bills</option>
+						<option value="39.900496398N,75.167165998W">Philadelphia Eagles</option>
+					</select>
+				</form>
+			</div>
+		)
+	}
+}
+TeamInput = connect()(TeamInput)
+
+export default TeamInput
