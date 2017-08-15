f53747509a7f1036887a1d740836f3f5ab37314a
Ryan Kelley <ryan.kelley@espn.com>
2017-08-15 18:26:36 -0400

    calculate the distance when 2 points are chosen

--- src/containers/Results.jsx
+++ src/containers/Results.jsx
@@ -2,13 +2,37 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Results extends Component  {
+	
+	componentDidMount() {
+		const { dispatch } = this.props
+	}
+
+	getDistance() {
+		if(this.props.searchLocation && this.props.teamLocation) {
+			let lat1 = this.props.searchLocation.latitude.radians, lon1 = this.props.searchLocation.longitude.radians,
+				lat2 = this.props.teamLocation.latitude.radians, lon2 = this.props.teamLocation.longitude.radians
+			let deltaLat = lat2-lat1, deltaLon = lon2-lon1
+			const a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon/2) * Math.sin(deltaLon/2)
+			const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+			const d = 6371 * c;
+			const m = d * .621371 
+			return (Math.round(d * 10) / 10 + ' kms to travel. (' + Math.round(m * 10) / 10 + ' miles)')
+		}
+		return ''
+	}
+
 	render() {
 		return(
 			<div>
-				You Could Go To This Place
+				{ this.getDistance() }
 			</div>
 		)
 	}
 }
 
-export default connect()(Results)
+const mapStateToProps = (state) => {
+	const { type, teamLocation, searchLocation } = state
+	return { type, teamLocation, searchLocation }
+}
+
+export default connect(mapStateToProps)(Results)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,15 @@
 import { combineReducers } from 'redux'
 
-const red = (state = [], action) => {
+const red = (state = { }, action) => {
 	switch(action.type) {
-		case 'FOO':
-			return [
-				...state,
-				{
-					foo: 'bar'
-				}
-			]
+		case 'TEAM_CHOSEN': 
+			return Object.assign({}, state, {
+				teamLocation: action.teamLocation
+			})
+		case 'LOCATION_CHOSEN':
+			return Object.assign({}, state, {
+				searchLocation: action.searchLocation
+			})
 		default:
 			return state
 	}
