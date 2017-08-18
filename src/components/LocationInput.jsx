332cb70c0f0d1b5a5fd47e16fda674894776f4a8
Ryan Kelley <ryan.kelley@espn.com>
2017-08-18 00:11:19 -0400

    adding a few more locations

--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -17,9 +17,13 @@ class LocationInput extends Component {
 						this.props.dispatch(chooseLocation(input.value))
 					}}>
 					<option value="-1"> -- Your Location --</option>
-					<option value="30.2672N,97.7431W">Austin, TX</option>
+					<option value="21.3069N,157.8583W">Honolulu, HI</option>
 					<option value="47.6062N,122.3321W">Seattle, WA</option>
+					<option value="32.7157N,117.1611W">San Diego, CA</option>
+					<option value="44.9778N,93.2650W">Minneapolis, MN</option>
+					<option value="30.2672N,97.7431W">Austin, TX</option>
 					<option value="41.7637N,72.6851W">Hartford, CT</option>
+					<option value="38.7209N,75.0760W">Rehoboth Beach, DE</option>
 				</select>
 
 
