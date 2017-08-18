63f96549695e3b8f7bce94833acb9a210135fcc7
Ryan Kelley <ryan.kelley@espn.com>
2017-08-17 21:22:30 -0400

    some stadium data

---
+++ src/data/stadiums.js
@@ -0,0 +1,39 @@
+const stadiums = {
+	"1": {teamId: 1, name:"Mercedes Benz Stadium", city: "Atlanta, Georgia", location: "33.75528N,84.40099W"},
+	"2": {teamId: 2, name:"New Era Stadium", city: "Something Orchard, NY", location: "42.77378N,78.78695W"},
+	"3": {teamId: 3, name:"Soldier Field", city: "Chicago, IL", location: "41.8623° N, 87.6167° W"},
+	"4": {teamId: 4, name:"Paul Brown Stadium", city: "Cincinatti, OH", location:"39.0955N,84.5161W" },
+	"5": {teamId: 5, name:"First Energy Stadium", city: "Cleveland, OH", location:"41.5061N,81.6995W"},
+	"6": {teamId: 6, name:"AT&T Stadium", city: "Arlington, TX", location: "32.7473N,97.0945W"},
+	"7": {teamId: 7, name:"Sports Authority Field", city: "Denver, CO", location:"39.7439° N, 105.0201° W"},
+	"8": {teamId: 8, name:"Ford Field", city: "Detroit, MI", location:"42.3400N,83.0456W"},
+	"9": {teamId: 9, name:"Lambeau Field", city: "Green Bay, WI", location:"44.5013N,88.0622W"},
+	"10": {teamId:10, name: "Nissan Stadium", city: "Nashville, TN", location:"36.1665N,86.7713W"},
+	"11": {teamId:11, name: "Lucas Oil Field", city:"Indianapolis, IN", location:"39.7560303092N,86.1582827002W"},
+	"12": {teamId:12, name: "Arrowhead Stadium", city: "Kansas City, MO", location:"39.042666496N,94.483664732W"},
+	"13": {teamId:13, name: "O.co", city: "Oakland, CA", location:"37.750996996N,122.200332532W"},
+	"14": {teamId:14, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+// if every body else moved to LA
+	"15": {teamId:15, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"16": {teamId:16, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"17": {teamId:17, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"18": {teamId:18, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"19": {teamId:19, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"20": {teamId:20, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"21": {teamId:21, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"22": {teamId:22, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"23": {teamId:23, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"24": {teamId:24, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"25": {teamId:25, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"26": {teamId:26, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"27": {teamId:27, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"28": {teamId:28, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"29": {teamId:29, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"30": {teamId:30, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"31": {teamId:33, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"},
+	"32": {teamId:34, name: "LA Col", city: "Los Angeles, CA", location: "34.008499966N,118.285998856W"}
+
+
+
+}
+module.exports = { stadiums }
